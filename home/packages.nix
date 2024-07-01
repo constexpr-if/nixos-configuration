c56c69887612c32128b8de6d3f81ca24f7f9abbd
constexpr12 <constexpr12@gmail.com>
2024-07-02 07:56:37 +0900

    add package joern

--- home/packages.nix
+++ home/packages.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  joern = pkgs.callPackage (fetchGit {
+    url = "git@github.com:constexpr-if/Joern-Nix-Package.git";
+    rev = "0dfe23b9b66c96bdc70064825f7347ba885866d3";
+  }) {};
+in {
   home.packages = with pkgs; [
     discord
     gparted
@@ -10,5 +15,6 @@
     vlc
     wget
     wineWowPackages.waylandFull
+    joern
   ];
 }
