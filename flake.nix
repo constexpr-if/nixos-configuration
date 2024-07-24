7991b2e24c1b3b15daf0f78a9c96004e42131740
constexpr12 <constexpr12@gmail.com>
2024-07-25 04:52:39 +0900

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,28 @@
 {
   description = "NixOS Configuration";
-  
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}: {
-   
-    nixosConfigurations.constDesktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/constDesktop/configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.constexpr12 = import ./home;
-        }
-      ];
+  outputs = { self, nixpkgs, home-manager, ...}:
+    let
+      mkHost = system: hostname: nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/${hostname}/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.constexpr12 = import ./home;
+          }
+        ];
+      };
+    in
+    {
+      nixosConfigurations.constDesktop = mkHost "x86_64-linux" "constDesktop";
+      nixosConfigurations.constLaptop  = mkHost "x86_64-linux" "constLaptop";
     };
-
-    nixosConfigurations.constLaptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/constLaptop/configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.constexpr12 = import ./home;
-        }
-      ];
-    };
-  };
 }
