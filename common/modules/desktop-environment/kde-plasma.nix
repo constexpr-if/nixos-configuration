3bd28ad8fe4027d38ac264ffda70a3698887fd8f
constexpr12 <constexpr12@gmail.com>
2024-07-28 15:58:00 +0900

    Fixed plasma6 not starting.
    Revert "disable wayland plasma..."
    
    This reverts commit 4a9fe7277414b070222a40ec5bd2a6a1f6e02451.
    
    Set x11 Plasma6 default session.

--- common/modules/desktop-environment/kde-plasma.nix
+++ common/modules/desktop-environment/kde-plasma.nix
@@ -2,7 +2,8 @@
   services.xserver.enable = true;
   services.displayManager.sddm = {
     enable = true;
-    #wayland.enable = true;
+    wayland.enable = true;
   };
   services.desktopManager.plasma6.enable = true;
+  services.displayManager.defaultSession = "plasmax11";
 }
