c577a187bda04cba94953e89fa2be88f1511eef3
constexpr-if <constexpr12@gmail.com>
2025-12-31 19:01:36 +0900

    Remove Iosevka

--- common/modules/fonts/default.nix
+++ common/modules/fonts/default.nix
@@ -1,14 +1,9 @@
 { pkgs, ... }:
 let
-  iosevka-custom = pkgs.iosevka.override {
-    set = "Custom";
-    privateBuildPlan = builtins.readFile ./iosevka.toml;
-  };
   iosevka-nerdfonts = pkgs.nerd-fonts.iosevka;
 in
 {
   fonts.packages = [
-    iosevka-custom
     iosevka-nerdfonts
     pkgs.noto-fonts-cjk-sans
     pkgs.pretendard
