8525ce5a32f102a3e920a9011463553b48fe9d9f
constexpr-if <constexpr12@gmail.com>
2026-01-14 21:18:07 +0900

    allow 25565 TCP port

--- common/modules/networking.nix
+++ common/modules/networking.nix
@@ -5,6 +5,10 @@
       "1.1.1.1"
       "1.0.0.1"
     ];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 25565 ];
+    };
     wireless.enable = false;
     networkmanager.enable = true;
   };
